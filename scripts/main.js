cb1f7d806003cf6c7a4e388a8196e39c20cd4632
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-04-21 10:24:09 -0500

    feat: Uploading and updating of files making up the customers section

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,38 @@
 console.log("[main.js] Test main.js");
+setTimeout(function () {
+
+    /* Image Slider - Swiper */
+    var imageSlider = new Swiper(".image-slider", {
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        loop: true,
+        spaceBetween: 50,
+        slidesPerView: 5,
+        breakpoints: {
+            // when window is <= 575px
+            575: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            },
+            // when window is <= 767px
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            // when window is <= 991px
+            991: {
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+            // when window is <= 1199px
+            1199: {
+                slidesPerView: 4,
+                spaceBetween: 20,
+            },
+        },
+    });
+
+
+},500);
