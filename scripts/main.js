cfd84c99fb39a1b20289faf79a22581ff5b7ba7b
AdrianAlzamoraG <u202015385@upc.edu.pe>
2022-04-23 21:54:29 -0500

    Feat: Updated index.html, main.css, main.js files, added Pricing section and applied styles.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
 console.log("[main.js] Test main.js");
 
 window.addEventListener("load", () => {
+    window.onscroll = function () {
+        scrollFunction();
+        scrollFunctionBTT(); // back to top button
+    };
+
+    window.onload = function () {
+        scrollFunction();
+    };
+    function scrollFunction() {
+        if (document.documentElement.scrollTop > 30) {
+            document
+                .getElementById("navbarExample")
+                .classList.add("top-nav-collapse");
+        } else if (document.documentElement.scrollTop < 30) {
+            document
+                .getElementById("navbarExample")
+                .classList.remove("top-nav-collapse");
+        }
+    }
     /* Image Slider - Swiper */
     new Swiper(".image-slider", {
         autoplay: {
@@ -33,4 +52,28 @@ window.addEventListener("load", () => {
             },
         },
     });
+    /* Back To Top Button */
+// Get the button
+    myButton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+    function scrollFunctionBTT() {
+        if (
+            document.body.scrollTop > 20 ||
+            document.documentElement.scrollTop > 20
+        ) {
+            myButton.style.display = "block";
+        } else {
+            myButton.style.display = "none";
+        }
+    }
+
+// When the user clicks on the button, scroll to the top of the document
+    document.getElementById("myBtn").onclick = topFunction;
+    function topFunction() {
+        document.body.scrollTop = 0; // for Safari
+        document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
+    }
 });
+
+
