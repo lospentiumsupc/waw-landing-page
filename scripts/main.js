cf8b9694bc02f4a30b1f80d9f8b5fb5214ac9f03
Diego Albitres <diego.albitres@gmail.com>
2022-05-01 06:44:24 -0500

    chore: removed back to top button
    * It doesn't belong to this PR

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,6 @@
 console.log("[main.js] Test main.js");
 
 window.addEventListener("load", () => {
-    window.onscroll = function () {
-        scrollFunction();
-        scrollFunctionBTT(); // back to top button
-    };
-
-    window.onload = function () {
-        scrollFunction();
-    };
-    function scrollFunction() {
-        if (document.documentElement.scrollTop > 30) {
-            document
-                .getElementById("navbarExample")
-                .classList.add("top-nav-collapse");
-        } else if (document.documentElement.scrollTop < 30) {
-            document
-                .getElementById("navbarExample")
-                .classList.remove("top-nav-collapse");
-        }
-    }
     /* Image Slider - Swiper */
     new Swiper(".image-slider", {
         autoplay: {
@@ -52,28 +33,4 @@ window.addEventListener("load", () => {
             },
         },
     });
-    /* Back To Top Button */
-// Get the button
-    myButton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-    function scrollFunctionBTT() {
-        if (
-            document.body.scrollTop > 10 ||
-            document.documentElement.scrollTop > 10
-        ) {
-            myButton.style.display = "block";
-        } else {
-            myButton.style.display = "none";
-        }
-    }
-
-// When the user clicks on the button, scroll to the top of the document
-    document.getElementById("myBtn").onclick = topFunction;
-    function topFunction() {
-        document.body.scrollTop = 0; // for Safari
-        document.documentElement.scrollTop = 0; // for Chrome, Firefox, IE and Opera
-    }
 });
-
-
