f78b47ed5b5f8fe783693cb24a949b2cf3686a4b
Diego Albitres <diego.albitres@gmail.com>
2022-06-11 03:52:14 -0500

    style: code formatting for main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,33 @@
-console.log("[main.js] Test main.js");
-
 window.addEventListener("load", () => {
-    /* Image Slider - Swiper */
-    new Swiper(".image-slider", {
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-        loop: true,
-        spaceBetween: 50,
-        slidesPerView: 5,
-        breakpoints: {
-            // when window is <= 575px
-            575: {
-                slidesPerView: 1,
-                spaceBetween: 10,
-            },
-            // when window is <= 767px
-            767: {
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            // when window is <= 991px
-            991: {
-                slidesPerView: 3,
-                spaceBetween: 20,
-            },
-            // when window is <= 1199px
-            1199: {
-                slidesPerView: 4,
-                spaceBetween: 20,
-            },
-        },
-    });
+  new Swiper(".image-slider", {
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    spaceBetween: 50,
+    slidesPerView: 5,
+    breakpoints: {
+      // when window is <= 575px
+      575: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      // when window is <= 767px
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      // when window is <= 991px
+      991: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      // when window is <= 1199px
+      1199: {
+        slidesPerView: 4,
+        spaceBetween: 20,
+      },
+    },
+  });
 });
